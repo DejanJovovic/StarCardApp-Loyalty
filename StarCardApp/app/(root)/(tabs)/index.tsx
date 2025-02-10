448375c6710a0900676803a58197de30df50ea89
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2025-02-10 16:51:14 +0100

    init with nativewind setup and assets

---
+++ StarCardApp/app/(root)/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+import {Link} from "expo-router";
+
+const Index = () => {
+    return (
+        <View style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+
+        }}>
+            <Text className="font-bold text-2xl my-10">Welcome to StarCard</Text>
+            <Link href="/sign-in">Sign in</Link>
+        </View>
+    )
+}
+export default Index
