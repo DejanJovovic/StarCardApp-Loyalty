bccee5a42a05d89bf326917e4c445ac96eed3a2c
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2025-05-10 12:20:39 +0200

    fixed conflicts between dependencies and probably fixed the error on settings screen with menu shrinking (needs to be tested)

--- StarCardApp/app/settings.tsx
+++ StarCardApp/app/settings.tsx
@@ -47,13 +47,14 @@ const SettingsItem = ({
 const Settings = () => {
     return (
         <SafeAreaView className="h-full">
-            <LinearGradient colors={[colors.gradientColor1, colors.gradientColor2]} className="h-full">
+            <LinearGradient colors={[colors.gradientColor1, colors.gradientColor2]} className="flex-1">
                 <View style={{position: "absolute", top: 0, left: 0, right: 0, zIndex: 1000}}>
                     <CustomHeaderLoggedIn/>
                 </View>
                 <ScrollView
+                    className="flex-1"
+                    contentContainerClassName="flex-grow px-7"
                     showsVerticalScrollIndicator={false}
-                    contentContainerClassName="px-7"
                 >
                     <View className="flex flex-col">
                         <View style={{marginTop: 76}}>
